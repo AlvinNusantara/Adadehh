a34a52e593e6465b17cc5cb4571f2aefe1184847
AlvinNusantara <bma36119@gmail.com>
2026-05-07 15:21:10 +0700

    Update bot token and owner ID in config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  tokenBot: "TOKENLU",
-  ownerID: "1082014738",
+  tokenBot: "8007735884:AAFJUFiLbpgxPAwrfSG4LNEBSkaCIiCPEv4",
+  ownerID: "8442277815",
   channelUsername: "@chanelch", // Pastikan pakai @ dan bot wajib dimasukkan ke chanel dan di adminkan yakkkkkk
 };
 
