0696446f7a1a48741c5341bcf2703066b4ad411c
AlvinNusantara <bma36119@gmail.com>
2026-05-07 13:43:04 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  tokenBot: "TOKENLU",
+  ownerID: "1082014738",
+  channelUsername: "@chanelch", // Pastikan pakai @ dan bot wajib dimasukkan ke chanel dan di adminkan yakkkkkk
+};
+
